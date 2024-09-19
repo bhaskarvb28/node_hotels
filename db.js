c99308ddf128b19999b9c9a2ee0b693abe5fe0ef
mogaveer <bhaskarvb28@gmail.com>
2024-09-19 18:26:44 +0530

    first version of our hotels

---
+++ db.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+//Define the MongoDB connection URL
+const mongoURL = `mongodb://localhost:27017/hotels` // Replace 'hotels' with your database name
+
+// Set up MongoDB connection
+// If you don't pass these parameters you'll get some error
+// MongoDB constantly upgrades itself
+mongoose.connect(mongoURL, /*{
+    useNewUrlParser: true, //Connection we establish using the URL is the new URL
+    useUnifiedTopology: true,
+}*/)
+
+//Mongoose defines or maintains a default connection object. That object is responsible always to perform or interact in any database connection
+// Get the default connection
+// Mongoose mainains a default connection object representing the MongoDB connection
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log("Connected to MongoDB server");
+})
+
+db.on('error', (err) => {
+    console.log("MongoDB connection error", err);
+})
+
+db.on('disconnected', () => {
+    console.log("MongoDB disconnected");
+})
+
+// Export the database connection
+
+module.exports = db;
---
+++ routes/menuItemRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router();
+
+
+const MenuItem = require('../models/MenuItem');
+
+router.get('/', async(req, res)=> {
+    try{
+      const data = await MenuItem.find();
+      console.log('Data fetched')
+      res.status(200).json(data);
+    }catch(err){
+      console.log(err);
+      res.status(500).json({error: "Internal Server Error"});
+    }
+})
+
+router.post('/', async(req, res) => {
+    try{
+        const data = req.body
+        const newMenu = new MenuItem(data);
+        const response = await newMenu.save();
+        console.log('Data Saved')
+        res.status(200).json(response);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({error: "Internal Server Error"});
+    }
+})
+
+router.get('/:taste', async (req, res) => {
+  try {
+    const foodTaste = req.params.taste;
+    if(foodTaste == 'sweet' || foodTaste == 'sour' || foodTaste == 'spicy') {
+      const response = await MenuItem.find({taste: foodTaste})
+      console.log(foodTaste, 'foods fetched')
+      res.status(200).json(response);
+    } else {
+      res.status(404).json("Invalid taste");
+    }
+  } catch (err) {
+      console.log(err);
+      res.status(500).json({error: "Internal Server Error"});
+  }
+})
+
+module.exports = router;
